b0c3b1f68d290090b48239b4cae856e1a7fff43f
goehmen <greg.oehmen@gmail.com>
2017-11-01 15:04:58 -0700

    adding 3rd pic of jerry

--- scripts/bak.main.js
+++ scripts/bak.main.js
@@ -4,8 +4,6 @@ myImage.onclick = function() {
     var mySrc = myImage.getAttribute('src');
     if(mySrc === 'images/jerry-garcia.jpg') {
       myImage.setAttribute ('src','images/jerry-garcia2.jpg');
-    } else if (mySrc === 'images/jerry-garcia2.jpg') {
-      myImage.setAttribute ('src','images/jerry-garcia3.jpg');
     } else {
       myImage.setAttribute ('src','images/jerry-garcia.jpg');
     }
